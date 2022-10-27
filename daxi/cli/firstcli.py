28aaeb679c27af4dade458c53c928a7620ad9a69
xiyu.yi <xiyu.yi@gmail.com>
2022-10-27 16:44:04 -0700

    This commit: set down the relationship between AcquisitionFcltr and the DeviceFcltr (command and receiver). next is to code in the pattern in both classes.
    2022-10-27
    today's todo:
    1. standardize the configurations, hard code the config file that contains all the configurations that allows the client to decide which type of FocusedProcessFcltr object to use, and allow the FocusedProcessFcltr, here in our first implementation, how to perform data acquisition.It will be stored in a yaml file, loaded out as a dictionary. [done].
    
    2. Think about how the AcquisitionFcltr (the command) would interact with the DeviceFcltr (receiver) that orchestrates all the devices.
    
    Note - always go back to the cli to look at the structure after each item. and read the notes there!
    
    3. Then think about how to isolate the DataFacilitator out (and how to leave a "port" for future expansion...).
    
    4. Then create a demo to perform the acquisition for a single stack.
    
    5. after that have a new series of commits for multi stack acquisiton.
    
    6. work on camera module for the rest of the week perhaps...

--- daxi/cli/firstcli.py
+++ daxi/cli/firstcli.py
@@ -1,7 +1,8 @@
 import click
 from daxi.ctr_processesfacilitator.acquisition.acquisition_facilitator import AcquisitionFcltr
 from daxi.ctr_devicesfacilitator.devicefacilitator import DevicesFcltr
-#todo figure out how to do the following later.
+
+# todo figure out how to do the following later.
 """
 eventually, would want to run the command line as:
 >>> daxi -c [configs file path] to perform a process pre-defined in a configuration file.
@@ -11,16 +12,17 @@ python cli/firstcli.py -c [path-to-configs]
 
 """
 
+
 @click.command()
 @click.option('--process', '-p', default='rconfigs', help='choose "rconfigs" to run from configuration file')
 @click.option('--configs_path', '-c', prompt='the full path of the configuration file',
               help='input the full path of the process configuration file')
-
-class CliInvoker():
+class CliInvoker:
     """
     this is the invoker type in the command pattern.
     perhaps for the sake of symmetry, we can also have GuiInvoker, or ther invokers.
     """
+
     def __init__(self):
         """
         history: command line interface history
@@ -36,43 +38,70 @@ class CliInvoker():
         :return:
         """
         self.history.append(command)
-        command.execute() # the focused process facilitators alls hould have an execute method. develop the abstraction
+        command.execute()  # the focused process facilitators alls hould have an execute method. develop the abstraction
         # when many focused process facilitators are implemented.
 
 
-def acquire(process_type, configs_path):
+def acquire(configs_path):
     """
-    this acquire() acts like a client in the "command pattern", it may serve the purpose of a ProcessFcltr
+    familiarize with with "command pattern" before you re-start implementing this module.
+    the client organizes the command, the receiver and the data. It gets the data, choose the appropriate command and
+    asks it to "execute". the command would take the data, and execute by driving the specific receivers.
 
-    it loads the configs that acts as data, and perform the acquisition (concrete command), which serves the purpose
+    ** Regarding client:
+    this acquire() acts like a client, it may serve the purpose of a ProcessFcltr (general purpose process fcltr).
+    It loads the configs that acts as data, and perform the acquisition (concrete command), which serves the purpose
     of focused facilitators.
-    will do abstraction for FocusedFcltr in the future when more focused facilitators are implemented.
+     - side note - will do abstraction for FocusedFcltr in the future when more focused facilitators are implemented.
 
+    ** Regarding data:
+    it is the configurations, specified in the configs_path file.
+    configs_path should be a yaml file, that has the configurations.
+    it will tell you the process type, the process configurations, and the device configurations.
+    Here we are implementing with mode1 acquisition as a starting point. the process configurations would contain
+    acquisition parameters (likewise, there should be calibration parameters, alignment parameters,
+    inspection parameters, etc.), and device parameters. (16 pieces of physical and virtual devices).
+
+    These configurations should be pared with the device_configs_core (when generate I guess?), as well as the alignment
+    and calibration records. configuration should not generate data, it should only store the configurations. unless the
+    data gets changed. but it should be reproducible with version control. - maybe save the numpy array later.
+    it should contain information enough for the client to know which focused process facilitator to use
+    it should contain all the specific acquisition parameter that is suggeste and chose from the SystemFcltr.
+    it should also contain the specific information for the command to handle all the receiveres. but this is specific for
+    some titles for the process.
+
+    ** Regarding receiver:
     The receiver is the DevicesFcltr - that contains the composition of all physical devices of a daxi microscope,
-    and drives the deives for one type of daq cycle. (change of daq cycle data should be done at the concrete command level,
-    (not the client level) - the focused processes fcltrs, and it drives the methods in the DevicesFcltr to start, stop,
-     re-write data, etc, and specifically controls the devices by calling th emethods offered by the DevicesFcltr.
-     The focused processes fcltrs also drives the focused data fcltrs through the general data facilitator type.
-    the implementation of all device fcltrs are currently on hold (need to use dexp and napari)
+    and drives the devices for one type of daq cycle. (change of daq cycle data should be done at the concrete command
+    level, (not the client level) - the focused processes fcltrs, and it drives the methods in the DevicesFcltr to
+    start, stop, re-write data, etc, and specifically controls the devices by calling th emethods offered by the
+    DevicesFcltr. The focused processes fcltrs also drives the focused data fcltrs through the general data facilitator
+    type. the implementation of all device fcltrs are currently on hold (need to use dexp and napari)
 
+    ** Regarding command:
     process fcltr (like a client in command pattern), will look at the chosen process,
     and ask general purpose DevicesFcltr and DataFcltr to provide
     suitable focused fcltrs (like concrete commands), each of which would access their receivers to
     perform specific process tasks.
 
+
     it seems like the command, object composition, and aggregation patterns all fit well here (probably...)
+
+
+
     """
     # receiver
-    device_fcltr = DeviceFcltr() # this is the receiver object
-    configs = get_configs(configs_path) # this is the data that is delivered to the receiver through the command.
-    acquisition = AcquisitionFcltr() # this is the command object. it does not create nor destroy the device_fcltr, it only access the device_fcltr. destroying
+    device_fcltr = DeviceFcltr()  # this is the receiver object
+    configs = get_configs(configs_path)  # this is the data that is delivered to the receiver through the command.
+    acquisition = AcquisitionFcltr()  # this is the command object. it does not create nor destroy the device_fcltr, it only access the device_fcltr. destroying
     # acquisition do not destroy the device_fcltr instances.
-
+    invoker = CliInvoker()
     invoker.invoke(concrete_command=acquisition(receiver=device_fcltr, data=configs))
 
     # invoker takes a concrete command that takes a receiver with it, it invokes the method of a receiver through the
     # execute() method in the concrete command.
     # the invoker also logs this execution.
+
 
 if __name__ == '__main__':
     acquire()
