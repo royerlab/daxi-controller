3932977f490096a0ef301d8c32e92b9fc016baf6
Xiyu Yi <xiyu.yi@czbiohub.org>
2022-11-01 12:19:03 -0700

    fixed bug: for tests that requries checkout daq cards, can't run in parallel. - put those tests in the same *.py file works... interesting. Clean up later. add decorator to the two new tests implemented on DaXi PC.

--- daxi/ctr_devicesfacilitator/tests/test_devicefcltr_load_and_run.py
+++ daxi/ctr_devicesfacilitator/tests/test_devicefcltr_load_and_run.py
@@ -1,4 +1,6 @@
 from daxi.ctr_devicesfacilitator.demos.demo_devicesfcltr_load_and_run import demo_devicefcltr_load_and_run
+from daxi.ctr_devicesfacilitator.demos.demo_devicesfcltr_receive_map_checkout_and_run import \
+    demo_devicefcltr_receive_map_checkout_and_run
 
 
 def test_devicefcltr_load_and_run():
@@ -6,3 +8,6 @@ def test_devicefcltr_load_and_run():
     assert msg is 'success'
 
 
+def test_devicefcltr_load_and_run():
+    msg = demo_devicefcltr_receive_map_checkout_and_run(verbose=False, interactive=False)
+    assert msg is 'success'
--- daxi/ctr_devicesfacilitator/tests/test_devicesfcltr_receive_map_checkout_and_run.py
+++ daxi/ctr_devicesfacilitator/tests/test_devicesfcltr_receive_map_checkout_and_run.py
@@ -2,6 +2,6 @@ from daxi.ctr_devicesfacilitator.demos.demo_devicesfcltr_receive_map_checkout_an
     demo_devicefcltr_receive_map_checkout_and_run
 
 
-def test_devicefcltr_load_and_run():
-    msg = demo_devicefcltr_receive_map_checkout_and_run(verbose=False, interactive=False)
-    assert msg is 'success'
+# def test_devicefcltr_load_and_run():
+#     msg = demo_devicefcltr_receive_map_checkout_and_run(verbose=False, interactive=False)
+#     assert msg is 'success'
