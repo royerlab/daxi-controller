f26fe276d286bb8d0d0be95eda033a3188ab145c
Xiyu Yi <xiyu.yi@czbiohub.org>
2022-11-30 16:13:07 -0800

    added demo for master pulse start trigger mode by using the camera module from copylot in DaXi-Controller.

--- daxi/cli/tests/test_firstcli.py
+++ daxi/cli/tests/test_firstcli.py
@@ -2,10 +2,10 @@ import pytest
 
 from daxi.cli.demos.demo_firstcli import demo_firstcli_acquire
 from daxi.globals_configs_constants_general_tools_needbettername.constants import process_configs_yaml_path
-from daxi.globals_configs_constants_general_tools_needbettername.python_globals import devices_connected
+from daxi.globals_configs_constants_general_tools_needbettername.python_globals import long_test
 
 
-@pytest.mark.skipif(devices_connected is False, reason="This test requires daq cards to be connected.")
+@pytest.mark.skipif(long_test is False, reason="This test requires daq cards to be connected.")
 def test_acquire():
     msg = demo_firstcli_acquire(process_configs_path=process_configs_yaml_path)
     assert msg == 'successful'
