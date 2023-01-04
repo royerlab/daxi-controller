d62b4dbf342cc5d24c1ec033053302c4d5ea6f3e
xiyu.yi <xiyu.yi@gmail.com>
2023-01-04 15:29:17 -0800

    modified counter module and added simulated counter.

--- daxi/control/device/facilitator/direct/orca_flash4_simulated.py
+++ daxi/control/device/facilitator/direct/orca_flash4_simulated.py
@@ -331,24 +331,6 @@ class OrcaFlash4Simulation:
         """
         pass
 
-    def retrieve_1_frame_from_1_camera(self, camera_id, frame_index):
-        # dcam = self.devices['camera '+str(camera_id)]
-        frame = self.devices['camera ' + str(camera_id)].buf_getframedata(frame_index)
-        return frame
-
-    def get_any_stack(self, camera_id, stack_index):
-        """This will return the last acquired stack as an numpy.ndarray"""
-        ind = stack_index
-        z = int(self.buffer_size_frame_number/3)
-        stack = np.ones([self.xdim, self.ydim, z], dtype='float')
-        for i in np.arange(z):
-            frame_index = ind*z + i
-            image = self.retrieve_1_frame_from_1_camera(camera_id=camera_id, frame_index=frame_index)
-            stack[:, :, i] = image
-
-        self.last_stack = stack
-        return self.last_stack
-
     def stop(self, camera_ids=[0]):
         """
         this will stop the capturing of the cameras
@@ -385,6 +367,24 @@ class OrcaFlash4Simulation:
         # Dcamapi.uninit()
         self.dcamapi_initiated = False
 
+    def retrieve_1_frame_from_1_camera(self, camera_id, frame_index):
+        # dcam = self.devices['camera '+str(camera_id)]
+        frame = self.devices['camera ' + str(camera_id)].buf_getframedata(frame_index)
+        return frame
+
+    def get_any_stack(self, camera_id, stack_index):
+        """This will return the last acquired stack as an numpy.ndarray"""
+        ind = stack_index
+        z = int(self.buffer_size_frame_number/3)
+        stack = np.ones([self.xdim, self.ydim, z], dtype='float')
+        for i in np.arange(z):
+            frame_index = ind*z + i
+            image = self.retrieve_1_frame_from_1_camera(camera_id=camera_id, frame_index=frame_index)
+            stack[:, :, i] = image
+
+        self.last_stack = stack
+        return self.last_stack
+
     def capturing_data_message(self, option, camera_ids=[0]):
         if option == 'on':
             for camera_id in camera_ids:
@@ -394,17 +394,18 @@ class OrcaFlash4Simulation:
             for camera_id in camera_ids:
                 self.devices['camera ' + str(camera_id)].message = False
 
-    def get_current_stack_index(self, camera_id):
-        frame_index = self.devices['camera ' + str(camera_id)].current_buffer_index
+    def get_current_stack_index(self, current_frame_count):
+        frame_index = current_frame_count
+        # self.devices['camera ' + str(camera_id)].current_buffer_index
         stack_index = int(frame_index/(int(self.buffer_size_frame_number/3)))
         self.current_stack_index = stack_index
         return stack_index
 
-    def get_current_stack(self, camera_id):
+    def get_current_stack(self, camera_id, current_frame_count):
         """This will return the last acquired stack as an numpy.ndarray"""
-        stack_index = self.get_current_stack_index(camera_id=camera_id)
+        stack_index = self.get_current_stack_index(current_frame_count=current_frame_count)
         self.get_any_stack(camera_id=camera_id, stack_index=stack_index)
-        print('current stack index is '+ str(stack_index))
+        print('current stack index is '+ str(current_frame_count))
         return self.last_stack
 
 
@@ -423,6 +424,7 @@ class SimulatedDcam():
         self.xdim = None
         self.ydim = None
         self.current_buffer_index = None
+        # may need to remove this out from the Dcam.
 
     def dev_open(self):
         self.device_opened = True
